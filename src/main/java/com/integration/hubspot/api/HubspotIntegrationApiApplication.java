26ae8f2ae02d85958f34489227895cc86e543994
Giovane <giovaneoliveira_ti@hotmail.com>
2025-03-31 16:41:44 -0300

    Commit inicial

---
+++ src/main/java/com/integration/hubspot/api/HubspotIntegrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.integration.hubspot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotIntegrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubspotIntegrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integration/hubspot/api/HubspotIntegrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.hubspot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotIntegrationApiApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
